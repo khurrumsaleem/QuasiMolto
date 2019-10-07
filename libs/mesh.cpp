68a52433781bb22b7a9bab2d15cd1351b876b496
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2019-10-07 15:30:24 -0400

    Added files where quadrature calculation will be performed

---
+++ libs/mesh.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+
+using namespace std;
+
+class Mesh
+{
+	public:
+	
+  	int n=12;		
+  	double* quadSet = NULL;
+  	double* mu = NULL;
+  	double* ordinates = NULL;
+  	
+  	void calcMu();	
+  	void calcQuadSet();
+	void printQuadSet();
+}
+
+void Mesh::calcMu(){
+		// Allocate memory and fix one degree of freedom
+    		mu = new int[n/2];
+		mu[0] = 0.1672126;
+		double constant = 2*(1-3*mu[1]**2)/(n-2);
+		for (int imu = 1; i < n; ++i){
+			mu[imu] = sqrt(mu[imu - 1]**2 + c);
+		}
+		cout << "mu(:) :"<<mu<<endl;
+}
+        
+void Mesh::calcQuadSet(){
+	calcMu();
+}
+
+void Mesh::printQuadSet(){        	
+	cout << "mu(:) :"<<mu<<endl;
+}	
---
+++ libs/mesh.h
@@ -0,0 +1,18 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include <iostream>
+
+class Mesh
+{
+	public:
+
+	int n;
+	double* quadSet;
+	double* mu;
+	double* ordinates;
+
+	void calcMu();
+	void calcQuadSet();
+	void printQuadSet();
+};
