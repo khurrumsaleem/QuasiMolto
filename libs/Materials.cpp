e70e1f0a4dd085391b44fbbc43faf955a93ccc4a
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2019-10-30 13:14:40 -0400

    Started making Materials class
    Added class definition and initialization function that defines
    pointers to the input yaml file and the mesh object

--- libs/Materials.cpp
+++ libs/Materials.cpp
@@ -1,11 +1,45 @@
-// test.cpp
+// File: Materials.cpp
+// Purpose: contain material characteristics and geometry for a simulation
+// Date: October 28, 2019
 
 #include <iostream>
+#include <cmath>
+#include <vector>
+#include <iomanip>
+#include <armadillo>
+#include "../TPLs/yaml-cpp/include/yaml-cpp/yaml.h"
+#include "Mesh.h"
 
-using namespace std;
+using namespace std; 
+using namespace arma;
 
-void printMat() {
+//==============================================================================
+//! Material class that holds material and geometry information
 
-     cout << "Printed from materials" << endl;
+class Materials
+{
+        public:
+        // public functions
+        Material(Mesh * myMesh,YAML::Node * myInput);
 
-}
+        private:
+        // private functions
+        YAML::Node * input;
+        Mesh * mesh;
+};
+
+//==============================================================================
+
+//==============================================================================
+//! Material class object constructor
+
+Materials::Materials(Mesh * myMesh,\
+                             YAML::Node * myInput)
+{
+	// Point to variables for mesh and input file
+	mesh = myMesh;
+	input = myInput;
+};
+
+//==============================================================================
+
