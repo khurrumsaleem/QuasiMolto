c6a161f624468ca996c36023253b2b38e47eeee5
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2020-02-06 15:11:29 -0500

    Added wrapper function to obtain all indices
    Also defined indices to access each face and center
    in the header file

--- libs/QuasidiffusionSolver.cpp
+++ libs/QuasidiffusionSolver.cpp
@@ -38,9 +38,10 @@ QDSolver::QDSolver(Mesh * myMesh,\
 
   // calculate number of unknowns  
   energyGroups = materials->nGroups;
-  nUnknowns = energyGroups*(5*(mesh->zCornerCent.size()\
-    *mesh->rCornerCent.size()) + 2*mesh->zCornerCent.size()\
-    + 2*mesh->rCornerCent.size());
+  nR = mesh->rCornerCent.size();
+  nZ = mesh->zCornerCent.size();
+  nGroupUnknowns = 5*(nZ*nR) + 2*nZ + 2*nR;
+  nUnknowns = energyGroups*nGroupUnknowns;
 
   // initialize size of linear system
   A.resize(nUnknowns,nUnknowns);
@@ -52,6 +53,8 @@ QDSolver::QDSolver(Mesh * myMesh,\
 //==============================================================================
 /// Form a portion of the linear system that belongs to SGQD 
 ///
+/// @param [in] SGQD quasidiffusion energy group to build portion of linear 
+///   for
 void QDSolver::formLinearSystem(SingleGroupQD * SGQD)	      
 {
 
@@ -60,5 +63,165 @@ void QDSolver::formLinearSystem(SingleGroupQD * SGQD)
 };
 
 //==============================================================================
+
+//==============================================================================
+/// Return global index of average flux at indices iR and iZ 
+/// @param [in] iR radial index of cell
+/// @param [in] iZ axial index of cell
+/// @param [out] index global index for average flux in cell at (iR,iZ)
+int QDSolver::CFluxIndex(int iR,int iZ,int energyGroup)
+{
+  int offset = energyGroup*nGroupUnknowns;
+  int index = iR + nR * (iZ);
+  return index;
+};
+
+//==============================================================================
+
+//==============================================================================
+/// Return global index of west face flux at indices iR and iZ 
+/// @param [in] iR radial index of cell
+/// @param [in] iZ axial index of cell
+/// @param [out] index global index for west face flux in cell at (iR,iZ)
+int QDSolver::WFluxIndex(int iR,int iZ,int energyGroup)
+{
+  int offset = energyGroup*nGroupUnknowns + nR*nZ;
+  int westIndex = offset + (iR) + (nR + 1) * iZ;
+
+  return westIndex;
+};
+
+//==============================================================================
+
+//==============================================================================
+/// Return global index of east face flux at indices iR and iZ 
+/// @param [in] iR radial index of cell
+/// @param [in] iZ axial index of cell
+/// @param [out] index global index for east face flux in cell at (iR,iZ)
+int QDSolver::EFluxIndex(int iR,int iZ,int energyGroup)
+{
+  int offset = energyGroup*nGroupUnknowns + nR*nZ;
+  int eastIndex = offset + (iR + 1) + (nR + 1) * iZ;
+
+  return eastIndex;
+};
+
+//==============================================================================
+
+//==============================================================================
+/// Return global index of north face flux at indices iR and iZ 
+/// @param [in] iR radial index of cell
+/// @param [in] iZ axial index of cell
+/// @param [out] index global index for north face flux in cell at (iR,iZ)
+int QDSolver::NFluxIndex(int iR,int iZ,int energyGroup)
+{
+  int offset = energyGroup*nGroupUnknowns + nR*nZ + (nR+1)*nZ;
+  int northIndex = offset + (iZ) + (nZ + 1) * iR;
+
+  return northIndex;
+};
+
+//==============================================================================
+
+//==============================================================================
+/// Return global index of south face flux at indices iR and iZ 
+/// @param [in] iR radial index of cell
+/// @param [in] iZ axial index of cell
+/// @param [out] index global index for south face flux in cell at (iR,iZ)
+int QDSolver::SFluxIndex(int iR,int iZ,int energyGroup)
+{
+  int offset = energyGroup*nGroupUnknowns + nR*nZ + (nR+1)*nZ;
+  int southIndex = offset + (iZ + 1) + (nZ + 1) * iR;
+
+  return southIndex;
+};
+
+//==============================================================================
+
+//==============================================================================
+/// Return global index of west face current at indices iR and iZ 
+/// @param [in] iR radial index of cell
+/// @param [in] iZ axial index of cell
+/// @param [out] index global index for west face current in cell at (iR,iZ)
+int QDSolver::WCurrentIndex(int iR,int iZ,int energyGroup)
+{
+  int offset = energyGroup*nGroupUnknowns + nR*nZ + (nR+1)*nZ + (nZ+1)*nR;
+  int westIndex = offset + (iR) + (nR + 1) * iZ;
+
+  return westIndex;
+};
+
+//==============================================================================
+
+//==============================================================================
+/// Return global index of east face current at indices iR and iZ 
+/// @param [in] iR radial index of cell
+/// @param [in] iZ axial index of cell
+/// @param [out] index global index for east face current in cell at (iR,iZ)
+int QDSolver::ECurrentIndex(int iR,int iZ,int energyGroup)
+{
+  int offset = energyGroup*nGroupUnknowns + nR*nZ + (nR+1)*nZ + (nZ+1)*nR;
+  int eastIndex = offset + (iR + 1) + (nR + 1) * iZ;
+
+  return eastIndex;
+};
+
+//==============================================================================
+
+//==============================================================================
+/// Return global index of north face current at indices iR and iZ 
+/// @param [in] iR radial index of cell
+/// @param [in] iZ axial index of cell
+/// @param [out] index global index for north face current in cell at (iR,iZ)
+int QDSolver::NCurrentIndex(int iR,int iZ,int energyGroup)
+{
+  int offset = energyGroup*nGroupUnknowns + nR*nZ + 2*(nR+1)*nZ + (nZ+1)*nR;
+  int northIndex = offset + (iZ) + (nZ + 1) * iR;
+
+  return northIndex;
+};
+
+//==============================================================================
+
+//==============================================================================
+/// Return global index of south face current at indices iR and iZ 
+/// @param [in] iR radial index of cell
+/// @param [in] iZ axial index of cell
+/// @param [out] index global index for south face current in cell at (iR,iZ)
+int QDSolver::SCurrentIndex(int iR,int iZ,int energyGroup)
+{
+  int offset = energyGroup*nGroupUnknowns + nR*nZ + 2*(nR+1)*nZ + (nZ+1)*nR;
+  int southIndex = offset + (iZ + 1) + (nZ + 1) * iR;
+
+  return southIndex;
+};
+
+//==============================================================================
+
+//==============================================================================
+/// Return global index of south face current at indices iR and iZ 
+/// @param [in] iR radial index of cell
+/// @param [in] iZ axial index of cell
+/// @param [out] index global index for south face current in cell at (iR,iZ)
+vector<int> QDSolver::indices(int iR,int iZ,int energyGroup)
+{
+  vector<int> indices;
+  indices.push_back(CFluxIndex(iR,iZ,energyGroup));
+  indices.push_back(WFluxIndex(iR,iZ,energyGroup));
+  indices.push_back(EFluxIndex(iR,iZ,energyGroup));
+  indices.push_back(NFluxIndex(iR,iZ,energyGroup));
+  indices.push_back(SFluxIndex(iR,iZ,energyGroup));
+  indices.push_back(WCurrentIndex(iR,iZ,energyGroup));
+  indices.push_back(ECurrentIndex(iR,iZ,energyGroup));
+  indices.push_back(NCurrentIndex(iR,iZ,energyGroup));
+  indices.push_back(SCurrentIndex(iR,iZ,energyGroup));
+
+  return indices;
+};
+
+//==============================================================================
+
+
+
 
 
--- libs/QuasidiffusionSolver.h
+++ libs/QuasidiffusionSolver.h
@@ -18,14 +18,30 @@ class QDSolver
     Materials * myMaterials,\
     YAML::Node * myInput);
   void formLinearSystem(SingleGroupQD * SGQD);
+  int CFluxIndex(int iR, int iZ, int energyGroup);
+  int WFluxIndex(int iR, int iZ, int energyGroup);
+  int EFluxIndex(int iR, int iZ, int energyGroup);
+  int NFluxIndex(int iR, int iZ, int energyGroup);
+  int SFluxIndex(int iR, int iZ, int energyGroup);
+  int WCurrentIndex(int iR, int iZ, int energyGroup);
+  int ECurrentIndex(int iR, int iZ, int energyGroup);
+  int NCurrentIndex(int iR, int iZ, int energyGroup);
+  int SCurrentIndex(int iR, int iZ, int energyGroup);
+  vector<int> indices(int iR, int iZ, int energyGroup);
+  
+  // public variables
   Eigen::SparseMatrix<double> A;
-  int energyGroups;
+  int energyGroups,nR,nZ,nGroupUnknowns;
 
   private:
-  // private functions
+  // private variables
   YAML::Node * input;
   Mesh * mesh;
   Materials * materials;
+  // indices for accessing index vector
+  int iCF = 0;
+  int iWF = 1, iEF = 2, iNF = 3, iSF = 4;
+  int iWC = 5, iEC = 6, iNC = 7, iSC = 8;
 };
 
 //==============================================================================
