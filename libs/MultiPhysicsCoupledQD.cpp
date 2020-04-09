751d6bb6d20d0a094894935b608e887eb4e56148
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2020-04-09 15:59:42 -0400

    Added empty class for MultiPhysicsCoupledQD
    This class will coordinate building the linear system to solve the
    multiphysics coupled quasidiffusion system by manipulating the
    HeatTransfer, MultiGroupPrecursor, and GreyGroupQD objects that
    it contains

---
+++ libs/MultiPhysicsCoupledQD.cpp
@@ -0,0 +1,19 @@
+// File: MultiPhysicsCoupledQD.cpp     
+// Purpose: Contains precursor, heat, and grey group quasidiffusion objects. 
+//   Also owns and is responsible for solving the coupled system each of 
+//   of those object contribute to.
+// Date: April 9, 2020
+
+#include "MultiPhysicsCoupledQD.h"
+
+using namespace std;
+
+//==============================================================================
+/// MultiPhysicsCoupledQD class object constructor
+///
+/// @param [in] blankType blank for this material
+MultiPhysicsCoupledQD::MultiPhysicsCoupledQD()
+{
+  // Assign inputs to their member variables
+};
+//==============================================================================
---
+++ libs/MultiPhysicsCoupledQD.h
@@ -0,0 +1,21 @@
+#ifndef MultiPhysicsCoupledQD_H
+#define MultiPhysicsCoupledQD_H
+
+#include "Mesh.h"
+
+using namespace std;
+
+//==============================================================================
+//! Contains precursor, heat, and grey group quasidiffusion objects.
+//!   Also owns and is responsible for solving the coupled system each of
+//!  of those object contribute to.
+
+class MultiPhysicsCoupledQD
+{
+  public:
+  MultiPhysicsCoupledQD();
+};
+
+//==============================================================================
+
+#endif
