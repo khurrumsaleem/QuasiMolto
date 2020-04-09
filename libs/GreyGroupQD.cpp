3572d8722cad0d9b799f13931eb74c6353261940
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2020-04-09 15:46:33 -0400

    Added empty class for grey group quasidiffusion
    This will be a derived class from SingleGroupTransport.cpp but
    with some function overwriting done to allow for the additional
    reaction term

---
+++ libs/GreyGroupQD.cpp
@@ -0,0 +1,17 @@
+// File: GreyGroupQD.cpp     
+// Purpose: Contains information and builds linear system for grey group qd
+// Date: April 9, 2020
+
+#include "GreyGroupQD.h"
+
+using namespace std;
+
+//==============================================================================
+/// GreyGroupQD class object constructor
+///
+/// @param [in] blankType blank for this material
+GreyGroupQD::GreyGroupQD()
+{
+  // Assign inputs to their member variables
+};
+//==============================================================================
---
+++ libs/GreyGroupQD.h
@@ -0,0 +1,19 @@
+#ifndef GreyGroupQD_H
+#define GreyGroupQD_H
+
+#include "Mesh.h"
+
+using namespace std;
+
+//==============================================================================
+//! Contains information and builds linear system for a grey group qd
+
+class GreyGroupQD
+{
+  public:
+  GreyGroupQD();
+};
+
+//==============================================================================
+
+#endif
