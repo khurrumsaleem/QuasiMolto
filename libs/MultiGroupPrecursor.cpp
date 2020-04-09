504f7cec5e33db1ed02af48b29e0e5ddafcb54d4
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2020-04-09 12:08:32 -0400

    Added empty MultiGroupPrecursor class
    The MultiGroupPrecursor class will contain all the
    SingleGroupPrecursor objects and any functions for looping over
    those single objects.

---
+++ libs/MultiGroupPrecursor.cpp
@@ -0,0 +1,17 @@
+// File: MultiGroupPrecursor.cpp     
+// Purpose: Container for all precursor groups
+// Date: April 9, 2020
+
+#include "MultiGroupPrecursor.h"
+
+using namespace std;
+
+//==============================================================================
+/// MultiGroupPrecursor class object constructor
+///
+/// @param [in] blankType blank for this material
+MultiGroupPrecursor::MultiGroupPrecursor()
+{
+  // Assign inputs to their member variables
+};
+//==============================================================================
---
+++ libs/MultiGroupPrecursor.h
@@ -0,0 +1,19 @@
+#ifndef MultiGroupPrecursor_H
+#define MultiGroupPrecursor_H
+
+#include "Mesh.h"
+
+using namespace std;
+
+//==============================================================================
+//! Container for all precursor group
+
+class MultiGroupPrecursor
+{
+  public:
+  MultiGroupPrecursor();
+};
+
+//==============================================================================
+
+#endif
