9dbe131a2102d59237e3520d9faddc484d3dcc00
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2020-04-09 12:33:21 -0400

    Added empty class for MGQDToMPQDCoupling
    This class will couple the multigroup quasidiffusion and
    multiphysics-coupled quasidiffuion objects

---
+++ libs/MultiGroupQDTOMultiPhysicsQDCoupling.cpp
@@ -0,0 +1,18 @@
+// File: MultiGroupQDToMultiPhysicsQDCoupling.cpp     
+// Purpose: Calculate coupling factors between multi-group quasidiffusion and
+//   multi-physics quasidiffusion.
+// Date: April 9, 2020
+
+#include "MultiGroupQDToMultiPhysicsQDCoupling.h"
+
+using namespace std;
+
+//==============================================================================
+/// MultiGroupQDToMultiPhysicsQDCoupling class object constructor
+///
+/// @param [in] blankType blank for this material
+MGQDToMPQDCoupling::MGQDToMPQDCoupling()
+{
+  // Assign inputs to their member variables
+};
+//==============================================================================
---
+++ libs/MultiGroupQDTOMultiPhysicsQDCoupling.h
@@ -0,0 +1,19 @@
+#ifndef MultiGroupQDToMultiPhysicsQDCoupling_H
+#define MultiGroupQDToMultiPhysicsQDCoupling_H
+
+#include "Mesh.h"
+
+using namespace std;
+
+//==============================================================================
+//! Container for all precursor group
+
+class MGQDToMPQDCoupling
+{
+  public:
+  MGQDToMPQDCoupling();
+};
+
+//==============================================================================
+
+#endif
