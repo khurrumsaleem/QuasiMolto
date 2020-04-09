81fa39617b49d31c2cb9503c18067229521add19
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2020-04-09 16:03:17 -0400

    Added empty class for HeatTransfer
    This class will hold information and build the linear system for
    the equations describing heat transfer

---
+++ libs/HeatTransfer.cpp
@@ -0,0 +1,17 @@
+// File: HeatTransfer.cpp     
+// Purpose: Contains information and builds linear system for heat transfer
+// Date: April 9, 2020
+
+#include "HeatTransfer.h"
+
+using namespace std;
+
+//==============================================================================
+/// HeatTransfer class object constructor
+///
+/// @param [in] blankType blank for this material
+HeatTransfer::HeatTransfer()
+{
+  // Assign inputs to their member variables
+};
+//==============================================================================
---
+++ libs/HeatTransfer.h
@@ -0,0 +1,19 @@
+#ifndef HeatTransfer_H
+#define HeatTransfer_H 
+
+#include "Mesh.h"
+
+using namespace std;
+
+//==============================================================================
+//! Contains information and builds linear system for heat transfer
+
+class HeatTransfer
+{
+  public:
+  HeatTransfer();
+};
+
+//==============================================================================
+
+#endif
