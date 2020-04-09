14713841ca627cebedcd042a918fef0a30e5e38b
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2020-04-09 11:54:27 -0400

    Added bare bones for SingleGroupPrecursor class
    This class will contain information and build the linear system
    for a single precursor group

---
+++ libs/SingleGroupPrecursor.cpp
@@ -0,0 +1,17 @@
+// File: SingleGroupPrecursor.cpp     
+// Purpose: Contains information for a precursor group
+// Date: April 9, 2020
+
+#include "SingleGroupPrecursor.h"
+
+using namespace std;
+
+//==============================================================================
+/// SingleGroupPrecursor class object constructor
+///
+/// @param [in] blankType blank for this material
+SingleGroupPrecursor::SingleGroupPrecursor()
+{
+  // Assign inputs to their member variables
+};
+//==============================================================================
---
+++ libs/SingleGroupPrecursor.h
@@ -0,0 +1,17 @@
+#ifndef SingleGroupPrecursor_H
+#define SingleGroupPrecursor_H
+
+using namespace std;
+
+//==============================================================================
+//! Contains information for a precursor group
+
+class SingleGroupPrecursor
+{
+  public:
+  SingleGroupPrecursor();
+};
+
+//==============================================================================
+
+#endif
