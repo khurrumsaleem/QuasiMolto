ba0733151c5a89c7de9de644e9985d410235999f
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2020-04-16 17:01:24 -0400

    CollapseXS - new class to hold averaged XSs
    The MG to GG coupling will handle calculating these... I think.

---
+++ libs/CollapsedCrossSections.cpp
@@ -0,0 +1,26 @@
+/// File: CollapsedCrossSections.cpp
+// Purpose: contain group collapsed nuclear data
+// Date: October 28, 2019
+
+#include "CollapsedCrossSections.h"
+
+using namespace std; 
+using namespace arma;
+
+///==============================================================================
+/// CollapsedCrossSections class object constructor
+///
+/// @param [in] myMats Materials object for this simulation
+CollapsedCrossSections::CollapsedCrossSections(int nZ,int nR)
+{
+  
+  // Initialize matrices holding one-group cross sections
+  sigT.setZero(nZ,nR);  
+  sigS.setZero(nZ,nR);  
+  sigF.setZero(nZ,nR);  
+  chiP.setZero(nZ,nR);  
+  chiD.setZero(nZ,nR);  
+
+};
+//==============================================================================
+
---
+++ libs/CollapsedCrossSections.h
@@ -0,0 +1,21 @@
+#ifndef COLLAPSEDCROSSSECTIONS_H
+#define COLLAPSEDCROSSSECTIONS_H
+
+#include "Mesh.h"
+
+using namespace std; 
+using namespace arma;
+
+//==============================================================================
+//! Class the holds collapsed one-group nuclear data
+
+class CollapsedCrossSections
+{
+        public:
+        CollapsedCrossSections(int nZ,int nR);
+	Eigen::MatrixXd sigT,sigS,sigF,chiP,chiD; 
+};
+
+//==============================================================================
+
+#endif
