c1510a0f67496e4409662ba0f6af969faa61f23c
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2020-04-20 11:23:49 -0400

    Driver - removed comment using deprecated variable

--- driver/driver.cpp
+++ driver/driver.cpp
@@ -133,7 +133,5 @@ void testMultiGroupPrecursor(Materials * myMaterials,\
   MultiGroupPrecursor * myMGP; 
   myMPQD = new MultiPhysicsCoupledQD(myMaterials,myMesh,input);
   cout << myMPQD->dnps->beta << endl;
-  cout << myMPQD->dnps->betas << endl;
-  cout << myMPQD->dnps->lambdas << endl;
   
 }
