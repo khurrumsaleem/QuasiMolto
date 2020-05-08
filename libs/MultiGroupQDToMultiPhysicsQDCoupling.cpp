2e898ffd787ced4801a236ad1680b54b5ee37588
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2020-05-08 18:06:49 -0400

    MG->GG - edit func, collapseNuclearData
    Added in loops for radial and axial current weighting

--- libs/MultiGroupQDToMultiPhysicsQDCoupling.cpp
+++ libs/MultiGroupQDToMultiPhysicsQDCoupling.cpp
@@ -40,7 +40,8 @@ void MGQDToMPQDCoupling::collapseNuclearData()
 
   // Temporary accumulator variables
   double fluxAccum,rCurrentAccum,zCurrentAccum,totalSigS;
-  double flux,rCurrent,zCurrent;
+  double flux,rCurrent,zCurrent,beta,nu;
+  double mySigT,mySigS,mySigF,myNeutV,myErr,myEzz,myErz;
 
   // Reset collapsed nuclear data
   mats->oneGroupXS->resetData();
@@ -53,72 +54,172 @@ void MGQDToMPQDCoupling::collapseNuclearData()
 
       // Reset accumulators
       fluxAccum = 0.0;
-      rCurrentAccum = 0.0;
-      zCurrentAccum = 0.0;
 
       // Loop over neutron energy groups
       for (int iEnergyGroup = 0; iEnergyGroup < mats->nGroups; iEnergyGroup++)
       {
         // Get flux and currents in this cell and energy group
         flux = mgqd->SGQDs[iEnergyGroup]->sFlux(iZ,iR); 
-        rCurrent = mgqd->SGQDs[iEnergyGroup]->currentR(iZ,iR); 
-        zCurrent = mgqd->SGQDs[iEnergyGroup]->currentZ(iZ,iR); 
+      
+        // Get beta and nu for this energy group 
+        beta = mpqd->mgdnp->beta(iEnergyGroup); 
+        nu = mats->nu(iZ,iR,iEnergyGroup); 
 
-        // Flux weighted sigT
-        mats->oneGroupXS->sigT(iZ,iR) = mats->sigT(iZ,iR,iEnergyGroup)*flux;
-
-        // Flux weighted sigS
-        totalSigS = 0; 
+        // Get nuclear data at these locations and energy groups
+        mySigT = mats->sigT(iZ,iR,iEnergyGroup);
+        mySigS= 0; 
         for (int iScat = 0; iScat < mats->nGroups; iScat++)
         {
-          totalSigS = totalSigS + mats->sigS(iZ,iR,0,iScat); 
+          mySigS = totalSigS + mats->sigS(iZ,iR,0,iScat); 
         }
-        mats->oneGroupXS->sigS(iZ,iR) = totalSigS*flux;
+        mySigF = mats->sigF(iZ,iR,iEnergyGroup);
+        myNeutV = mats->neutV(iEnergyGroup);
+        myErr = mgqd->SGQDs[iEnergyGroup]->Err(iZ,iR);
+        myEzz = mgqd->SGQDs[iEnergyGroup]->Ezz(iZ,iR);
+        myErz = mgqd->SGQDs[iEnergyGroup]->Erz(iZ,iR);
+
+        // Flux weighted sigT
+        mats->oneGroupXS->sigT(iZ,iR) = mySigT*flux;
+
+        // Flux weighted sigS
+        mats->oneGroupXS->sigS(iZ,iR) = mySigS*flux;
 
         // Flux weighted neutron velocity
-        mats->oneGroupXS->neutV(iEnergyGroup) = mats->neutV(iEnergyGroup)*flux;
+        mats->oneGroupXS->neutV(iZ,iR) = myNeutV*flux;
 
         // Flux weighted Ezz
-        mats->oneGroupXS->Ezz(iZ,iR) = mgqd->SGQDs[iEnergyGroup]->Ezz(iZ,iR)\
-                                       *flux;
+        mats->oneGroupXS->Ezz(iZ,iR) = myEzz*flux;
 
         // Flux weighted Err
-        mats->oneGroupXS->Err(iZ,iR) = mgqd->SGQDs[iEnergyGroup]->Err(iZ,iR)\
-                                       *flux;
+        mats->oneGroupXS->Err(iZ,iR) = myErr*flux;
 
         // Flux weighted Erz
-        mats->oneGroupXS->Erz(iZ,iR) = mgqd->SGQDs[iEnergyGroup]->Erz(iZ,iR)\
-                                       *flux;
+        mats->oneGroupXS->Erz(iZ,iR) = myErz*flux;
 
         // Flux weighted quasidiffusion coefficient
-        // Flux weighted DNP coefficient 
-
-        // Axial current weighted neutron velocity
-        // Axial current weighted sigTR
-
-        // Radial current weighted neutron velocity
-        // Radial current weighted sigTR
-
-        // Radial current weighted zeta1
-        // Radial current weighted zeta2
-
-        // Axial current weighted zeta1
-        // Axial current weighted zeta2
+        mats->oneGroupXS->qdFluxCoeff(iZ,iR) = (1-beta)*nu*mySigF*flux;
 
         // Loop over DNP groups
         for (int iDNPGroup = 0; iDNPGroup < mpqd->mgdnp->DNPs.size(); 
             iDNPGroup++) 
         {
           // Flux weighted DNP coefficient 
+          beta = mpqd->mgdnp->DNPs[iDNPGroup]->beta(iEnergyGroup);
+          mats->oneGroupXS->groupDNPFluxCoeff[iDNPGroup](iZ,iR)\
+            = beta*nu*mySigF*flux; 
         }
 
         // Accumulate flux
+        fluxAccum = fluxAccum + flux;
+        
+      }
+
+      // Divide data by accumulated values
+      mats->oneGroupXS->sigT(iZ,iR) = mats->oneGroupXS->sigT(iZ,iR)/fluxAccum;
+      mats->oneGroupXS->sigS(iZ,iR) = mats->oneGroupXS->sigS(iZ,iR)/fluxAccum;
+      mats->oneGroupXS->neutV(iZ,iR) = fluxAccum/mats->oneGroupXS->neutV(iZ,iR);
+      mats->oneGroupXS->Ezz(iZ,iR) = mats->oneGroupXS->Ezz(iZ,iR)/fluxAccum;
+      mats->oneGroupXS->Err(iZ,iR) = mats->oneGroupXS->Err(iZ,iR)/fluxAccum;
+      mats->oneGroupXS->Erz(iZ,iR) = mats->oneGroupXS->Erz(iZ,iR)/fluxAccum;
+      mats->oneGroupXS->qdFluxCoeff(iZ,iR) = mats->oneGroupXS->\
+                                             qdFluxCoeff(iZ,iR)/fluxAccum;
+
+      // Loop over DNP groups
+      for (int iDNPGroup = 0; iDNPGroup < mpqd->mgdnp->DNPs.size(); 
+          iDNPGroup++) 
+      {
+        mats->oneGroupXS->groupDNPFluxCoeff[iDNPGroup](iZ,iR)\
+          = mats->oneGroupXS->groupDNPFluxCoeff[iDNPGroup](iZ,iR)\
+          /fluxAccum; 
+      }
+
+    }
+  }
+
+  // Loop over spatial mesh
+  for (int iR = 0; iR < mesh->nR; iR++)
+  {
+    for (int iZ = 0; iZ < mesh->nZ+1; iZ++)
+    {
+      // Reset accumulator
+      zCurrentAccum = 0.0;
+      fluxAccum = 0.0;
+
+      // Loop over neutron energy groups
+      for (int iEnergyGroup = 0; iEnergyGroup < mats->nGroups; iEnergyGroup++)
+      {
+        // Get flux and currents in this cell and energy group
+        zCurrent = abs(mgqd->SGQDs[iEnergyGroup]->currentZ(iZ,iR));
+        flux = mgqd->SGQDs[iEnergyGroup]->sFluxZ(iZ,iR);
+
+        // Get nuclear data at these locations and energy groups
+        mySigT = mats->sigT(iZ,iR,iEnergyGroup);
+        myNeutV = mats->neutV(iEnergyGroup);
+
+        // Axial current weighted neutron velocity
+        mats->oneGroupXS->zSigTR(iZ,iR) = mySigT*zCurrent;
+
+        // Axial current weighted sigTR
+        mats->oneGroupXS->zNeutV(iEnergyGroup) = myNeutV*zCurrent;
+
+        // Axial current weighted zeta1
+        // Axial current weighted zeta2
+
         // Accumulate absolute axial current
-        // Accumulate absolute radial current
+        zCurrentAccum = zCurrentAccum + zCurrent;
         
       }
       // Divide data by accumulated values
+      mats->oneGroupXS->zSigTR(iZ,iR) = mats->oneGroupXS->zSigTR(iZ,iR)\
+                                        /zCurrentAccum;
+      mats->oneGroupXS->zNeutV(iZ,iR) = zCurrentAccum\
+                                        /mats->oneGroupXS->zNeutV(iZ,iR);
     }
   }
+
+  // Loop over spatial mesh
+  for (int iR = 0; iR < mesh->nR+1; iR++)
+  {
+    for (int iZ = 0; iZ < mesh->nZ; iZ++)
+    {
+
+      // Reset accumulators
+      rCurrentAccum = 0.0;
+      fluxAccum = 0.0;
+
+      // Loop over neutron energy groups
+      for (int iEnergyGroup = 0; iEnergyGroup < mats->nGroups; iEnergyGroup++)
+      {
+        // Get flux and currents in this cell and energy group
+        rCurrent = abs(mgqd->SGQDs[iEnergyGroup]->currentR(iZ,iR)); 
+        flux = mgqd->SGQDs[iEnergyGroup]->sFluxR(iZ,iR);
+      
+        // Get nuclear data at these locations and energy groups
+        mySigT = mats->sigT(iZ,iR,iEnergyGroup);
+        myNeutV = mats->neutV(iEnergyGroup);
+
+        // Radial current weighted sigTR
+        mats->oneGroupXS->rSigTR(iZ,iR) = mySigT*rCurrent;
+
+        // Radial current weighted neutron velocity
+        mats->oneGroupXS->rNeutV(iZ,iR) = myNeutV*rCurrent;
+
+        // Radial current weighted zeta1
+        // Radial current weighted zeta2
+
+        // Accumulate absolute radial current
+        rCurrentAccum = rCurrentAccum + rCurrent;
+        
+      }
+      // Divide data by accumulated values
+      mats->oneGroupXS->rSigTR(iZ,iR) = mats->oneGroupXS->rSigTR(iZ,iR)\
+                                        /rCurrentAccum;
+      mats->oneGroupXS->rNeutV(iZ,iR) = rCurrentAccum\
+                                        /mats->oneGroupXS->rNeutV(iZ,iR);
+      
+    }
+  }
+
+
 };
 //==============================================================================
