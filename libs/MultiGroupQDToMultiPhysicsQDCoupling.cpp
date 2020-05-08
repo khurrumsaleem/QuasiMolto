fd25563c147014827e4de4c3edda692f285e235e
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2020-05-08 12:53:00 -0400

    MG->GG - edit func, collapseNuclearData
    Added loops over mesh and neutron energy groups

--- libs/MultiGroupQDToMultiPhysicsQDCoupling.cpp
+++ libs/MultiGroupQDToMultiPhysicsQDCoupling.cpp
@@ -38,6 +38,52 @@ MGQDToMPQDCoupling::MGQDToMPQDCoupling(Mesh * myMesh,\
 void MGQDToMPQDCoupling::collapseNuclearData()
 {
 
+  // Temporary accumulator variables
+  double fluxAccum,rCurrentAccum,zCurrentAccum;
+
+  // Reset collapsed nuclear data
+  mats->oneGroupXS->resetData();
+
+  // Loop over spatial mesh
+  for (int iR = 0; iR < mesh->nR; iR++)
+  {
+    for (int iZ = 0; iZ < mesh->nZ; iZ++)
+    {
+
+      // Reset accumulators
+      fluxAccum = 0.0;
+      rCurrentAccum = 0.0;
+      zCurrentAccum = 0.0;
+
+      // Loop over neutron energy groups
+      for (int iEnergyGroup = 0; iEnergyGroup < mats->nGroups; iEnergyGroup++)
+      {
+        // Flux weighted sigT
+        // Flux weighted sigS
+        // Flux weighted neutron velocity
+        // Flux weighted Ezz
+        // Flux weighted Err
+        // Flux weighted Erz
+        // Flux weighted quasidiffusion coefficient
+        // Flux weighted DNP coefficient 
+        // Accumulate flux
+
+        // Axial current weighted neutron velocity
+        // Axial current weighted sigTR
+        // Accumulate axial current
+
+        // Radial current weighted neutron velocity
+        // Radial current weighted sigTR
+        // Accumulate radial current
+
+        // Radial current weighted zeta1
+        // Radial current weighted zeta2
+
+        // Axial current weighted zeta1
+        // Axial current weighted zeta2
+      }
+    }
+  }
 
 };
 //==============================================================================
