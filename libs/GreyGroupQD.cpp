e5153d41de512231cfde4011d443d3e7782c8f9f
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2020-09-01 12:32:48 -0400

    Style fix, GGQD, indenting
    Print statements didn't have correct indenting

--- libs/GreyGroupQD.cpp
+++ libs/GreyGroupQD.cpp
@@ -136,71 +136,70 @@ void GreyGroupQD::buildSteadyStateLinearSystem()
 };
 //==============================================================================
 
-
 //==============================================================================
 /// Print BC parameters 
 ///
 void GreyGroupQD::printBCParams()
 {
-cout << "eFluxBC: " << endl;
-cout << eFluxBC << endl;
-cout << endl;
+  cout << "eFluxBC: " << endl;
+  cout << eFluxBC << endl;
+  cout << endl;
 
-cout << "nFluxBC: " << endl;
-cout << nFluxBC << endl;
-cout << endl;
+  cout << "nFluxBC: " << endl;
+  cout << nFluxBC << endl;
+  cout << endl;
 
-cout << "sFluxBC: " << endl;
-cout << sFluxBC << endl;
-cout << endl;
+  cout << "sFluxBC: " << endl;
+  cout << sFluxBC << endl;
+  cout << endl;
 
-cout << "eInwardCurrentBC: " << endl;
-cout << eInwardCurrentBC << endl;
-cout << endl;
+  cout << "eInwardCurrentBC: " << endl;
+  cout << eInwardCurrentBC << endl;
+  cout << endl;
 
-cout << "nInwardCurrentBC: " << endl;
-cout << nInwardCurrentBC << endl;
-cout << endl;
+  cout << "nInwardCurrentBC: " << endl;
+  cout << nInwardCurrentBC << endl;
+  cout << endl;
 
-cout << "sInwardCurrentBC: " << endl;
-cout << sInwardCurrentBC << endl;
-cout << endl;
+  cout << "sInwardCurrentBC: " << endl;
+  cout << sInwardCurrentBC << endl;
+  cout << endl;
 
-cout << "eInwardFluxBC: " << endl;
-cout << eInwardFluxBC << endl;
-cout << endl;
+  cout << "eInwardFluxBC: " << endl;
+  cout << eInwardFluxBC << endl;
+  cout << endl;
 
-cout << "nInwardFluxBC: " << endl;
-cout << nInwardFluxBC << endl;
-cout << endl;
+  cout << "nInwardFluxBC: " << endl;
+  cout << nInwardFluxBC << endl;
+  cout << endl;
 
-cout << "sInwardFluxBC: " << endl;
-cout << sInwardFluxBC << endl;
-cout << endl;
+  cout << "sInwardFluxBC: " << endl;
+  cout << sInwardFluxBC << endl;
+  cout << endl;
 
-cout << "eOutwardCurrToFluxRatioBC: " << endl;
-cout << eOutwardCurrToFluxRatioBC << endl;
-cout << endl;
+  cout << "eOutwardCurrToFluxRatioBC: " << endl;
+  cout << eOutwardCurrToFluxRatioBC << endl;
+  cout << endl;
 
-cout << "nOutwardCurrToFluxRatioBC: " << endl;
-cout << nOutwardCurrToFluxRatioBC << endl;
-cout << endl;
+  cout << "nOutwardCurrToFluxRatioBC: " << endl;
+  cout << nOutwardCurrToFluxRatioBC << endl;
+  cout << endl;
 
-cout << "sOutwardCurrToFluxRatioBC: " << endl;
-cout << sOutwardCurrToFluxRatioBC << endl;
-cout << endl;
+  cout << "sOutwardCurrToFluxRatioBC: " << endl;
+  cout << sOutwardCurrToFluxRatioBC << endl;
+  cout << endl;
 
-cout << "eOutwardCurrToFluxRatioInwardWeightedBC: " << endl;
-cout << eOutwardCurrToFluxRatioInwardWeightedBC << endl;
-cout << endl;
+  cout << "eOutwardCurrToFluxRatioInwardWeightedBC: " << endl;
+  cout << eOutwardCurrToFluxRatioInwardWeightedBC << endl;
+  cout << endl;
 
-cout << "nOutwardCurrToFluxRatioInwardWeightedBC: " << endl;
-cout << nOutwardCurrToFluxRatioInwardWeightedBC << endl;
-cout << endl;
+  cout << "nOutwardCurrToFluxRatioInwardWeightedBC: " << endl;
+  cout << nOutwardCurrToFluxRatioInwardWeightedBC << endl;
+  cout << endl;
 
-cout << "sOutwardCurrToFluxRatioInwardWeightedBC: " << endl;
-cout << sOutwardCurrToFluxRatioInwardWeightedBC << endl;
-cout << endl;
+  cout << "sOutwardCurrToFluxRatioInwardWeightedBC: " << endl;
+  cout << sOutwardCurrToFluxRatioInwardWeightedBC << endl;
+  cout << endl;
 };
 //==============================================================================
 
@@ -210,41 +209,41 @@ cout << endl;
 void GreyGroupQD::printEddingtons()
 {
 
-cout << "Err: " << endl;
-cout << Err << endl;
-cout << endl;
+  cout << "Err: " << endl;
+  cout << Err << endl;
+  cout << endl;
 
-cout << "Ezz: " << endl;
-cout << Ezz << endl;
-cout << endl;
+  cout << "Ezz: " << endl;
+  cout << Ezz << endl;
+  cout << endl;
 
-cout << "Erz: " << endl;
-cout << Erz << endl;
-cout << endl;
+  cout << "Erz: " << endl;
+  cout << Erz << endl;
+  cout << endl;
 
-cout << "ErrAxial: " << endl;
-cout << ErrAxial << endl;
-cout << endl;
+  cout << "ErrAxial: " << endl;
+  cout << ErrAxial << endl;
+  cout << endl;
 
-cout << "EzzAxial: " << endl;
-cout << EzzAxial << endl;
-cout << endl;
+  cout << "EzzAxial: " << endl;
+  cout << EzzAxial << endl;
+  cout << endl;
 
-cout << "ErzAxial: " << endl;
-cout << ErzAxial << endl;
-cout << endl;
+  cout << "ErzAxial: " << endl;
+  cout << ErzAxial << endl;
+  cout << endl;
 
-cout << "ErrRadial: " << endl;
-cout << ErrRadial << endl;
-cout << endl;
+  cout << "ErrRadial: " << endl;
+  cout << ErrRadial << endl;
+  cout << endl;
 
-cout << "EzzRadial: " << endl;
-cout << EzzRadial << endl;
-cout << endl;
+  cout << "EzzRadial: " << endl;
+  cout << EzzRadial << endl;
+  cout << endl;
 
-cout << "ErzRadial: " << endl;
-cout << ErzRadial << endl;
-cout << endl;
+  cout << "ErzRadial: " << endl;
+  cout << ErzRadial << endl;
+  cout << endl;
 
 };
 //==============================================================================
