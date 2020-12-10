628e397e6122d5b7d56fb95d9ae07ba2c1138fb4
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2020-12-10 10:51:59 -0500

    Updated tests to initialize PETSc

--- tests/src/sgQuasidiffusionTest.cpp
+++ tests/src/sgQuasidiffusionTest.cpp
@@ -5,11 +5,14 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
+  PetscInitialize(&argc,&argv,(char*)0,"Test");
+  
   YAML::Node * input;
   input = new YAML::Node;
   *input = YAML::LoadFile("inputs/1-group-test.yaml");
+  PetscErrorCode ierr;
 
   // initialize mesh object
   Mesh * myMesh;
@@ -26,4 +29,6 @@ int main()
   // initialize single group transport object
   SingleGroupQD * mySGQD;
   mySGQD = new SingleGroupQD(0,myMGQD,myMaterials,myMesh,input);
+  
+  ierr = PetscFinalize();
 }
