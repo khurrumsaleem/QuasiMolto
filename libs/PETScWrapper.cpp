eec9416af452c5f00e1e356eda07a4311695097a
Aaron J. Reynolds <reynolaa@oregonstate.edu>
2020-12-09 19:19:59 -0500

    feature - convert to petsc vector
    Added a function to convert an eigen vector into a petsc vector

--- libs/PETScWrapper.cpp
+++ libs/PETScWrapper.cpp
@@ -28,6 +28,31 @@ int initPETScMat(Mat *A, int squareSize, int nonZeros)
 
 }
 
+int initPETScRectMat(Mat *A, int rows, int cols, int nonZeros)
+{
+  PetscErrorCode ierr; 
+
+  /* Initialize matrix */
+  ierr = MatCreate(PETSC_COMM_WORLD,A);
+  CHKERRQ(ierr);
+  
+  /* Set size of matrix */
+  ierr = MatSetSizes(*A,PETSC_DECIDE,PETSC_DECIDE,rows,cols);
+  CHKERRQ(ierr);
+  
+  /* Pull in command line options */
+  ierr = MatSetFromOptions(*A);
+  CHKERRQ(ierr);
+  
+  /* Preallocate matrix memory */
+  ierr = MatMPIAIJSetPreallocation(*A,nonZeros,NULL,nonZeros,NULL);
+  CHKERRQ(ierr);
+  ierr = MatSeqAIJSetPreallocation(*A,nonZeros,NULL);
+  CHKERRQ(ierr);
+
+}
+
+
 int initPETScVec(Vec *x, int size)
 {
   PetscErrorCode ierr;
@@ -45,5 +70,18 @@ int initPETScVec(Vec *x, int size)
   CHKERRQ(ierr);
   
 }
+
+int eigenVecToPETScVec(Eigen::VectorXd *x_e,Vec *x_p)
+{
+  PetscErrorCode ierr;
+  
+  for (int idx = 0; idx < (*x_e).size(); idx++)
+  {
+  ierr = VecSetValue(*x_p,idx,(*x_e)(idx),INSERT_VALUES);CHKERRQ(ierr); 
+  }
+  ierr = VecAssemblyBegin(*x_p);CHKERRQ(ierr);
+  ierr = VecAssemblyEnd(*x_p);CHKERRQ(ierr);
+}
+
 
 //==============================================================================
--- libs/PETScWrapper.h
+++ libs/PETScWrapper.h
@@ -2,13 +2,16 @@
 #define PETSCWRAPPER_H 
 
 #include <petsc.h>
+#include "../TPLs/eigen-git-mirror/Eigen/Eigen"
 
 using namespace std;
 
 //==============================================================================
 
 int initPETScMat(Mat *A, int squareSize, int nonZeros);
+int initPETScRectMat(Mat *A, int rows, int cols, int nonZeros);
 int initPETScVec(Vec *A, int size);
+int eigenVecToPETScVec(Eigen::VectorXd *x_e,Vec *x_p);
 
 //==============================================================================
 
